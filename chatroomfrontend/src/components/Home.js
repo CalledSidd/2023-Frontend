c3a38f4103a242a7bef2e21cc6e372fed97dc2b8
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-15 23:50:52 +0530

    reac13

---
+++ chatroomfrontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='bottom-11 fixed'>
+        <div className="border boredr-white w-10/12 h-96">
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
