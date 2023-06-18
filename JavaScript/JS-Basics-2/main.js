b1c8927aa03e7732311c17935d17e6cacf21b2fe
CalledSidd <sidharthsidhu12317@gmail.com>
2023-06-18 14:58:22 +0530

    9

--- JavaScript/JS-Basics-2/main.js
+++ JavaScript/JS-Basics-2/main.js
@@ -7,7 +7,8 @@ let lastName = 'S';
 
 const interestRate = 0.3;
 
-interestRate = 1;
+// interestRate = 1; 
+// the line above would be an error, constants cannot be reassigned if a variable requires re assign make it let
 // the value of a variable can change but constants can't be changed
 
 console.log(interestRate)
