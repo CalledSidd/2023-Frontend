49e1b06352e8171d2071b03f162b5e03858051af
CalledSidd <sidharthsidhu12317@gmail.com>
2023-06-18 14:57:08 +0530

    7

--- JavaScript/JS-Basics-2/main.js
+++ JavaScript/JS-Basics-2/main.js
@@ -1,2 +1,13 @@
 let name = 'Sidharth'
 console.log(name)
+
+let firstName = 'Sidharth';
+
+let lastName = 'S';
+
+let interestRate = 0.3;
+
+interestRate = 1;
+// the value of a variable can change but constants can't be changed
+
+console.log(interestRate)
