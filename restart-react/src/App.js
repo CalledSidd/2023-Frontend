2a510948ec05328026fb109826a017ceff57fe99
CalledSidd <sidharthsidhu12317@gmail.com>
2023-07-08 16:51:12 +0530

    26

--- restart-react/src/App.js
+++ restart-react/src/App.js
@@ -2,10 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='title'>
-        Pokemon Search
-      </h1>
+    <div className="App" 
+    style={{
+        margin: 'auto',
+        width: 800,
+        paddingTop: "1rem"
+      }}
+    >
+      <h1 className='title'>Pokemon Search</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Pikachoo</td>
+            <td>Bulbasaur</td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 }
